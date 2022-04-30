764c8a0d66f9b2c0cf4315f5b2b061dbd5bbf270
ThanhNguyenDat <johnkelvin3107@gmail.com>
2022-05-01 00:41:08 +0700

    Update index.js morgan

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express');
+const morgan = require('morgan')
 const app = express();
 const port = 3000
 
-app.get('/', (req, res) => {
-    var a = 1
-    var b = 2
-    var c = a + b
-    
-    res.send('Hello World!')
+app.use(morgan('combined'))
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
 })
 
 app.listen(port, () => console.log(`Example app listening on  http://localhost:${port}`))
